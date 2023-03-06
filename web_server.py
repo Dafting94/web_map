789f357f0cebbb5634d6983af635dba2bb688f2f
Mag József <mag.jozsef@dmjvph.net>
2023-03-06 09:56:57 +0100

    my debrecen_website

---
+++ web_server.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template, request, redirect
+import csv
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_home():
+    return render_template ("index.html")
+'''
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template (page_name)
+'''
